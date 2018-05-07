cda1679187b3e87461baba0392a960d6b15c734b
dinhhoi1102 <dinhhoi1102@gmail.com>
2018-05-07 14:27:18 +0700

    version1

---
+++ src/main/java/com/aht/QlnsApplication.java
@@ -0,0 +1,12 @@
+package com.aht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QlnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QlnsApplication.class, args);
+	}
+}
---
+++ src/main/java/com/aht/entities/Training.java
@@ -0,0 +1,5 @@
+package com.aht.entities;
+
+public class Training {
+
+}
