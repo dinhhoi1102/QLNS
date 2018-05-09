f0744b78fde3002f6dc5bb221d4f001ad541080e
dinhhoi1102 <dinhhoi1102@gmail.com>
2018-05-09 15:06:55 +0700

    config update

--- src/main/java/com/aht/QlnsApplication.java
+++ src/main/java/com/aht/QlnsApplication.java
@@ -1,12 +1,17 @@
 package com.aht;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
+
 public class QlnsApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(QlnsApplication.class, args);
 	}
 }
